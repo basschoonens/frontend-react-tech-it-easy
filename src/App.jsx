950e86f7d56a31bca01c417e1b819c6220d6184a
basschoonens <bas.schoonens@gmail.com>
2024-01-28 20:03:58 +0100

    styling bijgewerkt - nog afmaken

--- src/App.jsx
+++ src/App.jsx
@@ -17,32 +17,43 @@ function App() {
 
     return (
         <>
-        <h1>Begin hier met met maken van de applicatie!</h1>
-            <p>Het totale aantal verkocht is:</p><p className={"soldCount"}>{soldCount}</p>
-            <p>Het totale aantal ingekochte tv's is:</p><p className={"purchasedCount"}>{purchasedCount}</p>
-            <p>Het totale nog te verkopen tv's is:</p><p className={"totalStock"}>{totalStock}</p>
-            <p>{tvDescription}</p>
-            <p>{tvPrice}</p>
-            <p>{screenSize}</p>
-
-            <div className="outer-container">
-                <img src={bestSellingTv.sourceImg} alt={"bestselling-tv"}/>
-                <article className="inner-container">
-                    <p>{tvDescription}</p>
-                    <p>{tvPrice}</p>
-                    <p>{screenSize}</p>
-                    <div className="feature-buttons">
-                        <img className="buttons" src={check}/><p>wifi</p>
-                        <img className="buttons" src={minus}/><p>speech</p>
-                        <img className="buttons" src={check}/><p>hdr</p>
-                        <img className="buttons" src={check}/><p>bluetooth</p>
-                        <img className="buttons" src={minus}/><p>ambilight</p>
-                    </div>
-                    <button onClick={handleClick}>Meest verkocht eerst</button>
-                    <button onClick={handleClick}>Goedkoopste eerst</button>
-                    <button onClick={handleClick}>Meest geschikt voor sport eerst</button>
-                </article>
+        <h1>Tech it easy dashboard</h1>
+            <p>Verkoopoverzicht</p>
+            <div className="outer-stats-container">
+                <div className="sold-stat">
+                    <p>Aantal verkochte producten</p>
+                    <p className={"soldCount"}>{soldCount}</p>
+                </div>
+                <div className="purchased-stat">
+                    <p>Aantal ingekochte producten</p>
+                    <p className={"purchasedCount"}>{purchasedCount}</p>
+                </div>
+                <div className="stock-stat">
+                    <p>Aantal te verkopen producten</p>
+                    <p className={"totalStock"}>{totalStock}</p>
+                </div>
             </div>
+            <p>Best verkochte tv</p>
+            <div className="outer-container">
+                <span className="tv-image-wrapper">
+                        <img className="tv-image" src={bestSellingTv.sourceImg} alt={"bestselling-tv"}/>
+                </span>
+                    <article className="inner-container">
+                        <h1>{tvDescription}</h1>
+                        <h2>{tvPrice}</h2>
+                        <h3>{screenSize}</h3>
+                        <div className="feature-buttons">
+                            <img className="buttons" src={check}/><p>wifi</p>
+                            <img className="buttons" src={minus}/><p>speech</p>
+                            <img className="buttons" src={check}/><p>hdr</p>
+                            <img className="buttons" src={check}/><p>bluetooth</p>
+                            <img className="buttons" src={minus}/><p>ambilight</p>
+                        </div>
+                    </article>
+            </div>
+            <button onClick={handleClick}>Meest verkocht eerst</button>
+            <button onClick={handleClick}>Goedkoopste eerst</button>
+            <button onClick={handleClick}>Meest geschikt voor sport eerst</button>
         </>
     )
 }
