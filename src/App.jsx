acdeae5c2e50436b5c62d414154821dc22d0a8b8
basschoonens <bas.schoonens@gmail.com>
2024-01-31 22:55:38 +0100

    huiswerk af, inleveren

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import './App.css';
-import {bestSellingTv, inventory} from "./constants/inventory.js";
+import {inventory, bestSellingTv} from "./constants/inventory.js";
 import soldCount from "./helpers/totalSold.js";
 import purchasedCount from "./helpers/totalPurchased.js";
 import totalStock from "./helpers/totalStock.js";
@@ -11,50 +11,52 @@ import minus from "./assets/minus.png";
 
 function App() {
 
-        const handleClick = (event) => {
-            console.log(event.target.innerText);
-        }
+    const handleClick = (event) => {
+        console.log(event.target.innerText);
+    }
 
     return (
-        <>
-        <h1>Tech it easy dashboard</h1>
+        <main className="page-container">
+            <h1>Tech it easy dashboard</h1>
             <p>Verkoopoverzicht</p>
             <div className="outer-stats-container">
-                <div className="sold-stat">
+                <div className="sold-stat stats">
                     <p>Aantal verkochte producten</p>
-                    <p className={"soldCount"}>{soldCount}</p>
+                    <p className="soldCount">{soldCount}</p>
                 </div>
-                <div className="purchased-stat">
+                <div className="purchased-stat stats">
                     <p>Aantal ingekochte producten</p>
-                    <p className={"purchasedCount"}>{purchasedCount}</p>
+                    <p className="purchasedCount">{purchasedCount}</p>
                 </div>
-                <div className="stock-stat">
+                <div className="stock-stat stats">
                     <p>Aantal te verkopen producten</p>
-                    <p className={"totalStock"}>{totalStock}</p>
+                    <p className="totalStock">{totalStock}</p>
                 </div>
             </div>
             <p>Best verkochte tv</p>
-            <div className="outer-container">
-                <span className="tv-image-wrapper">
-                        <img className="tv-image" src={bestSellingTv.sourceImg} alt={"bestselling-tv"}/>
-                </span>
-                    <article className="inner-container">
-                        <h1>{tvDescription}</h1>
-                        <h2>{tvPrice}</h2>
-                        <h3>{screenSize}</h3>
-                        <div className="feature-buttons">
-                            <img className="buttons" src={check}/><p>wifi</p>
-                            <img className="buttons" src={minus}/><p>speech</p>
-                            <img className="buttons" src={check}/><p>hdr</p>
-                            <img className="buttons" src={check}/><p>bluetooth</p>
-                            <img className="buttons" src={minus}/><p>ambilight</p>
-                        </div>
-                    </article>
+            <div className="outer-tv-container">
+                    <span className="tv-image-wrapper">
+                            <img className="tv-image" src={bestSellingTv.sourceImg} alt={"bestselling-tv"}/>
+                    </span>
+                <article className="inner-tv-container">
+                    <h1>{tvDescription}</h1>
+                    <h2>{tvPrice}</h2>
+                    <h3>{screenSize}</h3>
+                    <div className="features-container">
+                        <img className="icon" src={check}/><p>wifi</p>
+                        <img className="icon" src={minus}/><p>speech</p>
+                        <img className="icon" src={check}/><p>hdr</p>
+                        <img className="icon" src={check}/><p>bluetooth</p>
+                        <img className="icon" src={minus}/><p>ambilight</p>
+                    </div>
+                </article>
             </div>
-            <button onClick={handleClick}>Meest verkocht eerst</button>
-            <button onClick={handleClick}>Goedkoopste eerst</button>
-            <button onClick={handleClick}>Meest geschikt voor sport eerst</button>
-        </>
+            <div className="buttons-container">
+                <button onClick={handleClick}>Meest verkocht eerst</button>
+                <button onClick={handleClick}>Goedkoopste eerst</button>
+                <button onClick={handleClick}>Meest geschikt voor sport eerst</button>
+            </div>
+        </main>
     )
 }
 
